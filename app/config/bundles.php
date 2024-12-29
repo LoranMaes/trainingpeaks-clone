fe639f7c69893cf55a800b6ee9bd7982dbb0c7f2
Loran Maes <loran.maes@aware.be>
2024-12-29 12:25:46 +0100

    Registration flow made

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -13,4 +13,6 @@ return [
     Symfony\UX\React\ReactBundle::class => ['all' => true],
     Symfony\UX\Chartjs\ChartjsBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
 ];
